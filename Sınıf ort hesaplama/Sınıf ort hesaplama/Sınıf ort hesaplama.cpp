47b97c7242cf27f41c37d651d03c0225ba639f61
erenpolat3622 <127830405+erenpolat3622@users.noreply.github.com>
2024-03-10 20:35:06 +0300

    Add files via upload

---
+++ Sınıf ort hesaplama/Sınıf ort hesaplama/Sınıf ort hesaplama.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+int  b = 0;
+int average(int a) {
+    b = a + b;
+
+        return b;
+}
+int main()
+{
+    int a[100], i = 0, c, w, k;
+    float x;
+    while( i<= 100) {
+        again:
+        printf("enter a grade ");
+        scanf_s("%d", &a[i]);
+        w=average(a[i]);
+        printf("if you want enter new grade press 1 otherwise press 0 ");
+        scanf_s("%d", &c);
+        if (c == 1) {
+            i++;
+            goto again;
+        }
+        else {
+            k = i + 1;
+            x = w /k  ;
+            printf("Your class average grade is %f", x);
+                 
+        }
+
+    }
+
+
+}
+
